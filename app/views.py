2860208d28e5004753b731372e6227af7cfcc4ac
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-02 12:09:54 +0200

    Final

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Food, Consume
 
 def index(request):
@@ -10,9 +10,17 @@ def index(request):
         consume = Consume(user=user, food_consumed=consume)
         consume.save()
         foods = Food.objects.all()
+        return redirect('/')
     else:
         foods = Food.objects.all()
     
     consumed_food = Consume.objects.filter(user=request.user)
     context = {'foods': foods, 'consumed_food': consumed_food}
     return render(request, 'index.html', context)
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return  redirect('index')
+    return render(request, 'delete.html')
