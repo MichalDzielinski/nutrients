e1ae7583c5282ef3aab7e4677d502ac19a0c365f
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-09-02 06:31:13 +0200

    Another one

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Food, Consume
 
-# Create your views here.
+def index(request):
+
+    if request.method == 'POST':
+        food_consumed = request.POST['food_consumed']
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+    else:
+        foods = Food.objects.all()
+    context = {'foods': foods}
+    return render(request, 'index.html', context)
